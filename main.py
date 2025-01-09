8fb4e6d7dce99e2343a57cd49f6461b10fed4924
Ben Schreiber <bpschreiber2003@gmail.com>
2025-01-09 11:20:13 -0800

    cleanup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import logging
 import os
-import random
 import threading
 import time
 from typing import Optional
@@ -14,10 +14,17 @@ from dotenv import load_dotenv
 INSTAGRAM_URL = "https://www.instagram.com/"
 REELS_URL = "https://www.instagram.com/reels/"
 
+skip_reel = threading.Event()
+
 # Environment variables
 load_dotenv()
 email = os.getenv("EMAIL")
 password = os.getenv("PASSWORD")
+
+# Logging
+logging.basicConfig(
+    level=logging.ERROR, format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
 
 class ThreadSafeWebDriver:
@@ -42,74 +49,28 @@ class ThreadSafeWebDriver:
         self.lock.release()
 
 
-def login(driver, user, password) -> None:
-    def input_creds(d, user, password) -> None:
-        user_form = WebDriverWait(d, 15).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, "input[name='username']"))
-        )
-        user_form.send_keys(user)
-        password_form = WebDriverWait(d, 15).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, "input[name='password']"))
-        )
-        password_form.clear()
-        password_form.send_keys(password)
+def reel_scroller(tswd: ThreadSafeWebDriver):
+    def video_duration(driver: webdriver.Chrome, video_index: int) -> Optional[float]:
+        """Finds the duration of the video on screen and returns it in seconds"""
+        videos = driver.find_elements(By.TAG_NAME, "video")
+        v: Optional[float] = None
+        try:
+            v = videos[video_index]
+        finally:
+            return None if not v else float(v.get_attribute("duration"))
 
-    def submit(d) -> None:
-        WebDriverWait(d, 10).until(
-            EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']"))
-        ).click()  # Login
+    def next_reel(driver: webdriver.Chrome):
+        # TODO: Arrow down doesnt always register until page is manually scrolled
+        body = driver.find_element(By.TAG_NAME, "body")
+        body.send_keys(Keys.ARROW_DOWN)
 
-        time.sleep(10)  # Wait for login
-
-    input_creds(driver, user, password)
-    submit(driver)
-
-
-def video_duration(d, video_index) -> Optional[float]:
-    """Finds the duration of the video on screen and returns it in seconds"""
-    videos = d.find_elements(By.TAG_NAME, "video")
-    print(f"Length of videos:{len(videos)}; Index {video_index}")
-    v = None
-    try:
-        v = videos[video_index]
-    finally:
-        return None if not v else float(v.get_attribute("duration"))
-
-
-def next_reel(d):
-    body = d.find_element(By.TAG_NAME, "body")
-    body.send_keys(Keys.ARROW_DOWN)
-
-
-def like_reel(d, video_index):
-    like_svgs = d.find_elements(By.CSS_SELECTOR, 'svg[aria-label="Like"]')
-    try:
-        svg = like_svgs[video_index]
-        actions = ActionChains(d)
-        actions.move_to_element(svg).click().perform()
-
-    except Exception as e:
-        print(e)
-
-
-def follow_user(d, video_index):
-    follow_buttons = d.find_elements(By.XPATH, "//*[normalize-space(text())='Follow']")
-    try:
-        follow_button = follow_buttons[video_index]
-        actions = ActionChains(d)
-        actions.move_to_element(follow_button).click().perform()
-    except Exception as e:
-        print(e)
-
-
-def reel_scroller(driver: ThreadSafeWebDriver):
     while True:
-        duration = None
-        with driver as d:
-            WebDriverWait(d, 10).until(
+        duration: Optional[float] = None
+        with tswd as driver:
+            WebDriverWait(driver, 10).until(
                 EC.presence_of_element_located((By.CSS_SELECTOR, "video"))
             )
-            duration = video_duration(d, driver.video_index)
+            duration = video_duration(driver, tswd.video_index)
 
         if duration is None:
             time.sleep(1)  # Instagram is fetching new video
@@ -118,36 +79,83 @@ def reel_scroller(driver: ThreadSafeWebDriver):
         skip_reel.wait(duration)
         skip_reel.clear()
 
-        with driver as d:
-            next_reel(d)
-        driver.video_index += 1
+        with tswd as driver:
+            next_reel(driver)
+
+        tswd.video_index += 1
 
 
-def input_listener(driver: ThreadSafeWebDriver):
-    # TODO: voice input
+def get_user_input():
+    return input()  # TODO: Voice command input
+
+
+def input_listener(tswd: ThreadSafeWebDriver):
+    """Runs commands based on some user input"""
+
+    def follow_user(driver: webdriver.Chrome, video_index: int):
+        follow_buttons = driver.find_elements(
+            By.XPATH, "//*[normalize-space(text())='Follow']"
+        )
+        try:
+            follow_button = follow_buttons[video_index]
+            actions = ActionChains(driver)
+            actions.move_to_element(follow_button).click().perform()
+        except Exception as e:
+            logging.error(e)
+
+    def like_reel(driver: webdriver.Chrome, video_index: int):
+        like_svgs = driver.find_elements(By.CSS_SELECTOR, 'svg[aria-label="Like"]')
+        try:
+            svg = like_svgs[video_index]
+            actions = ActionChains(driver)
+            actions.move_to_element(svg).click().perform()
+        except Exception as e:
+            logging.error(e)
+
     while True:
-        inp = input()
+        inp = get_user_input()
         if inp == "s":
             skip_reel.set()
-            print("Skipping")
+            logging.log(logging.INFO, "Skipping")
         elif inp == "l":
-            with driver as d:
-                like_reel(d, driver.video_index)
-            print("Liking")
+            with tswd as driver:
+                like_reel(driver, tswd.video_index)
+            logging.log(logging.INFO, "Liking")
         elif inp == "f":
-            with driver as d:
-                follow_user(d, driver.video_index)
-            print("Following")
+            with tswd as driver:
+                follow_user(driver, tswd.video_index)
+            logging.log(logging.INFO, "Following")
 
 
-skip_reel = threading.Event()
+def login(driver: webdriver.Chrome, user: str, password: str):
+    def input_creds():
+        user_form = WebDriverWait(driver, 15).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "input[name='username']"))
+        )
+        user_form.send_keys(user)
+        password_form = WebDriverWait(driver, 15).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "input[name='password']"))
+        )
+        password_form.clear()
+        password_form.send_keys(password)
+
+    def submit():
+        WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']"))
+        ).click()  # Login
+
+        time.sleep(6)  # Wait for login
+
+    input_creds()
+    submit()
+
 
 if __name__ == "__main__":
-    driver = ThreadSafeWebDriver()
-    with driver as d:
-        d.get(INSTAGRAM_URL)
-        login(d, email, password)
-        d.get(REELS_URL)
+    tswd = ThreadSafeWebDriver()
+    with tswd as driver:
+        driver.get(INSTAGRAM_URL)
+        login(driver, email, password)
+        driver.get(REELS_URL)
 
-    threading.Thread(target=reel_scroller, args=(driver,)).start()
-    threading.Thread(target=input_listener, args=(driver,)).start()
+    threading.Thread(target=reel_scroller, args=(tswd,)).start()
+    threading.Thread(target=input_listener, args=(tswd,)).start()
